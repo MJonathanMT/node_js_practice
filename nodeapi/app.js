0d44ea9cb734bb01464c33955ffe2cda6c880f1a
M. Jonathan D Tirtapraja <mjonathanmt@gmail.com>
2023-01-29 18:49:44 -0500

    get request

---
+++ nodeapi/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+
+const postRoutes = require('./routes/post');
+app.use(morgan("dev"));
+
+app.use("/", postRoutes);
+
+const port = "8080";
+app.listen(port, () => {
+    console.log(`Server is listening on port: ${port}`);
+});
